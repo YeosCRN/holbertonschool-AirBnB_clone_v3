d3063e78878f6a00ba8d31663cbe2e7071d3e2a7
Chrs-creyk <chrisacount117@gmail.com>
2023-04-14 16:42:16 -0500

    upgrading init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Init file for flask views"""
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -10,5 +11,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
